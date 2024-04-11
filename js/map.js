cbe19f22e4e73018a53019dbdba52d0413d16b76
VictoriaVann <156718723+VictoriaVann@users.noreply.github.com>
2024-04-10 19:57:46 -0500

    Created map.js

---
+++ js/map.js
@@ -0,0 +1,12 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15.5,
+    center: { lat: 29.749907, lng: -95.358421},
+  });
+  var marker = new google.maps.Marker({
+    position: { lat: 29.749907, lng: -95.358421},
+    map: map,
+  });
+  infowindow.open(map, marker);
+}
+window.initMap = initMap;
